423ebd908c15bb31de64f81cf4c1dbd867883fa0
danimdev <106762674+danimdev@users.noreply.github.com>
2022-06-16 04:52:25 +0200

    changed arround with the binding

--- NotesApp/MainWindow.xaml.cs
+++ NotesApp/MainWindow.xaml.cs
@@ -20,13 +20,14 @@ namespace NotesApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<ToDoItem> list = new List<ToDoItem>();
+
         public MainWindow()
         {
             InitializeComponent();
-
-            List<ToDoItem> list = new List<ToDoItem>();
             list.Add(new ToDoItem() { Title = "Make a new Program",Description = "Make it versatile" }) ;
-            list.Add(new ToDoItem() { Title = "Make a Game", Description = "Let it be a shooter" });
+            list.Add(new ToDoItem() { Title = "Make a Game", Description = "Let it be a shooter and make it like so you can shoot other enemys" });
+            list.Add(new ToDoItem() { Title = "This is an extra long title to see how it would look like", Description = "test" });
             icTodolist.ItemsSource = list;
         }
 
@@ -34,6 +35,12 @@ namespace NotesApp
         {
             public string Title { get; set; }
             public string Description { get; set; }
+        }
+
+        private void DeleteActualElement(object sender, RoutedEventArgs e)
+        {
+            //remove item from list
+            list.RemoveAt(icTodolist.);
         }
     }
 }
